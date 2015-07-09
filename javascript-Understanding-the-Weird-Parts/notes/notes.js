d057abaa4f2faaed9921a8a805b9273c217a2886
sosana <thesosana@gmail.com>
2015-07-09 18:48:14 +0000

    udpating notes on syntax parsers, execution context, and lexical environments

--- javascript-Understanding-the-Weird-Parts/notes/notes.js
+++ javascript-Understanding-the-Weird-Parts/notes/notes.js
@@ -1,5 +1,16 @@
 /**
  *  
- * add notes here
+ * Syntax parser:
+ *  - a program that reads your code and determines what it does and if its grammer is valid
+ *  - someone else wrote a program to translate it for the computer, ex: a compiler
  * 
+ * Lexical Environment:
+ *  - where something sits physically in the code your write
+ *  - 'lexical' means 'having to do with words or grammar'. A lexical environment exists in 
+ *    programming languages in which where you write something is important
+ *  
+ *  Execution Context:
+ *  - a wrapper to help manage the code that is running.
+ *  - there are lots of lexical environments. Which one is currently running is managed via 
+ *    execution contexts. It can contain things beyond what you've written in your code
  */
