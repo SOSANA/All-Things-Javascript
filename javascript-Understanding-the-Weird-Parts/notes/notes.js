e01cfdf270ec1ee1f11166a8e5e751c04b1cb77c
sosana <thesosana@gmail.com>
2015-07-09 19:47:58 -0600

    updating notes on name value pairs and objects

--- javascript-Understanding-the-Weird-Parts/notes/notes.js
+++ javascript-Understanding-the-Weird-Parts/notes/notes.js
@@ -1,16 +1,38 @@
 /**
- *  
+ *
  * Syntax parser:
  *  - a program that reads your code and determines what it does and if its grammer is valid
  *  - someone else wrote a program to translate it for the computer, ex: a compiler
- * 
+ *
  * Lexical Environment:
  *  - where something sits physically in the code your write
- *  - 'lexical' means 'having to do with words or grammar'. A lexical environment exists in 
+ *  - 'lexical' means 'having to do with words or grammar'. A lexical environment exists in
  *    programming languages in which where you write something is important
- *  
- *  Execution Context:
+ *
+ * Execution Context:
  *  - a wrapper to help manage the code that is running.
- *  - there are lots of lexical environments. Which one is currently running is managed via 
+ *  - there are lots of lexical environments. Which one is currently running is managed via
  *    execution contexts. It can contain things beyond what you've written in your code
+ *
+ * Name Value Pairs:
+ *  - a name which maps to a unique value
+ *  - the name may be defined more than once, but only can have one calue in any given context
+ *  - That value may be more name/value pairs
+ *  - example var address = '100 main street';
+ *  	'address' is the name and '100 main street' is the value
+ *
+ * Object:
+ * 	- a collection of name value pairs
+ * 	= the simplest definition when talking about javascript
+ *  - example is address and apartment is a collection of name value pairs
+ *  	address:
+ *  	{
+ *  		street: 'Main',
+ *  		number: 100
+ *  		apartment:
+ *  		{
+ *  			floor: 3,
+ *  			number: 301
+ *  		}
+ *  	}
  */
