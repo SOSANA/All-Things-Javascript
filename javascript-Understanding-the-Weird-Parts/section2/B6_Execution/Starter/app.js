a84a4a96b4f47e8fb476db45bf3db518d000d390
sosana <thesosana@gmail.com>
2015-07-11 20:50:26 +0000

    updating the execution context second phase, the execution phase

--- javascript-Understanding-the-Weird-Parts/section2/B6_Execution/Starter/app.js
+++ javascript-Understanding-the-Weird-Parts/section2/B6_Execution/Starter/app.js
@@ -1,9 +1,12 @@
-b();
-
-console.log(a);
-
-var a = 'Hello World!';
-
 function b() {
     console.log('Called b!');
 }
+// calling function b
+b(); // Called b!
+// logging a
+console.log(a); // undefined
+
+// setting a to a value and declaring it
+var a = 'Hello World!';
+// logging a
+console.log(a); // Hello World!
