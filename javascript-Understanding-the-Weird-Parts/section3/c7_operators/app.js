677e904c4c281b8df9e78f86ec673f38a980d0a5
sosana <thesosana@gmail.com>
2015-07-12 21:00:59 +0000

    testing some operator functions

---
+++ javascript-Understanding-the-Weird-Parts/section3/c7_operators/app.js
@@ -0,0 +1,11 @@
+// this plus sign is an operator and is actually a function
+// the js engine provides the ability to write whats is called infix notation, means 
+// the operator sits between two parameters ex: var a below
+var a = 4 + 3;
+/*
+ * whats happening behind the scenes
+ * function +(a, b) {
+ *  return // add the two #'s
+ * }
+ */
+console.log(a);
