34b2050f119ff6962562ee06a3763898def46c43
sosana <thesosana@gmail.com>
2015-07-12 20:46:00 +0000

    updating notes on primitive types

--- javascript-Understanding-the-Weird-Parts/section3/notes/notes.js
+++ javascript-Understanding-the-Weird-Parts/section3/notes/notes.js
@@ -1,5 +1,22 @@
 /**
- *  
- * add notes here
  * 
+ * Dynamic Typing:
+ *  - You don't tell the engine what type of data a variable holds, it figures out while
+ *    your code is running
+ *  - variables can hold different types of values because it's all figured out during 
+ *    execution
+ *  - figures it out on the fly
+ * 
+ * Primitive Types:
+ *  - a type of data that represents a single value
+ *  - something that is not an object
+ *  - there are 6 types of primitives
+ *      - UNDEFINED: represents lack of existence, happens when we never set the value 
+ *      - Null: represents lack of existence, when you want your variable to equal nothing
+ *      - BOOLEAN: true or false, one of two values
+ *      - NUMBER: js only has one numeric type, "floating point number" (there's always some 
+ *        decimals) Unlike other programming languages, there's only one 'number' type and it 
+ *        can make math weird
+ *      - STRING: a sequence of characters (both single '' and double "" quotes can be used)
+ *      - SYMBOL: Used in ES6 (the next version of Javascript)
  */
