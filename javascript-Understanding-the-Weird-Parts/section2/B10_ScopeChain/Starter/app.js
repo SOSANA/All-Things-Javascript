fa44f77b3ac6538489938997225ff6c421d1ea3d
sosana <thesosana@gmail.com>
2015-07-12 19:28:00 +0000

    creating functions, global/local vars to test lexical environment and scope chaining

--- javascript-Understanding-the-Weird-Parts/section2/B10_ScopeChain/Starter/app.js
+++ javascript-Understanding-the-Weird-Parts/section2/B10_ScopeChain/Starter/app.js
@@ -1,9 +1,17 @@
-function b() {
-	console.log(myVar);
-}
+/*
+ * Ask the question who created me? when thinking of lexical environment and scope chaining
+ * during the create phase of execution context. Where a function sits, determines its outter
+ * environment reference but as its being executed those execution context are stacking up
+ * and its running synchronous
+ */
 
 function a() {
-	var myVar = 2;
+  
+  function b() {
+	  console.log(myVar);
+  }
+  
+  var myVar = 2;
 	b();
 }
 
