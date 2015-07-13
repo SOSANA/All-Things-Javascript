eaa1529141f09d75becdbf1e6693531f00086b99
sosana <thesosana@gmail.com>
2015-07-12 19:28:12 -0600

    updating

--- javascript-Understanding-the-Weird-Parts/section3/notes/notes.js
+++ javascript-Understanding-the-Weird-Parts/section3/notes/notes.js
@@ -1,30 +1,30 @@
 /**
- * 
+ *
  * Dynamic Typing:
  *  - You don't tell the engine what type of data a variable holds, it figures out while
  *    your code is running
- *  - variables can hold different types of values because it's all figured out during 
+ *  - variables can hold different types of values because it's all figured out during
  *    execution
  *  - figures it out on the fly
- * 
+ *
  * Primitive Types:
  *  - a type of data that represents a single value
  *  - something that is not an object
  *  - there are 6 types of primitives
- *      - UNDEFINED: represents lack of existence, happens when we never set the value 
+ *      - UNDEFINED: represents lack of existence, happens when we never set the value
  *      - Null: represents lack of existence, when you want your variable to equal nothing
  *      - BOOLEAN: true or false, one of two values
- *      - NUMBER: js only has one numeric type, "floating point number" (there's always some 
- *        decimals) Unlike other programming languages, there's only one 'number' type and it 
+ *      - NUMBER: js only has one numeric type, "floating point number" (there's always some
+ *        decimals) Unlike other programming languages, there's only one 'number' type and it
  *        can make math weird
  *      - STRING: a sequence of characters (both single '' and double "" quotes can be used)
  *      - SYMBOL: Used in ES6 (the next version of Javascript)
- * 
+ *
  * Operator:
  *  - operators are functions
  *  - a special function that is syntactically (written) differently
  *  - generally, operators take two parameters and return one result
- *  - the js engine provides the ability to write whats is called infix notation, means the operator 
+ *  - the js engine provides the ability to write whats is called infix notation, means the operator
  *    sits between two parameters
  *  - common operators are +, -, > (greater than), < (less than)
  */
