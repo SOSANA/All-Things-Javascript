e5d96c5a2d4546b02918076c98d26e34b7e080dd
sosana <thesosana@gmail.com>
2015-07-12 19:46:26 -0600

    updating

---
+++ javascript-Understanding-the-Weird-Parts/section3/c7_operatorsPrecedenceAndAssociativity/app.js
@@ -0,0 +1,3 @@
+
+var a = 4 + 3 * 5;
+console.log(a);
--- javascript-Understanding-the-Weird-Parts/section3/notes/notes.js
+++ javascript-Understanding-the-Weird-Parts/section3/notes/notes.js
@@ -1,7 +1,14 @@
 /**
+ * Associativity:
+ * 	- what order operator functions get called in: left to right (called left associativity) or
+ * 		right to left (called right associativity)
+ * 	- when functions have the same precedence
+ * 	- ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+ * 			- the one with the biggest number will get called first and the one with the smaller second as parameters
+ * 				pdf in /notes folder and mdn link above
  *
  * Dynamic Typing:
- *  - You don't tell the engine what type of data a variable holds, it figures out while
+ *  - you don't tell the engine what type of data a variable holds, it figures out while
  *    your code is running
  *  - variables can hold different types of values because it's all figured out during
  *    execution
@@ -27,4 +34,8 @@
  *  - the js engine provides the ability to write whats is called infix notation, means the operator
  *    sits between two parameters
  *  - common operators are +, -, > (greater than), < (less than)
+ *
+ * Operator Precedence:
+ * 	- which operator function gets called first
+ *  - functions are called in order of precedence (HIGHER precedence wins)
  */
