c1990d2b13bec54d402be3d83ec5756301f70cf9
sosana <thesosana@gmail.com>
2015-07-12 20:06:19 -0600

    testing out operators precedence and associativity

--- javascript-Understanding-the-Weird-Parts/section3/c7_operatorsPrecedenceAndAssociativity/app.js
+++ javascript-Understanding-the-Weird-Parts/section3/c7_operatorsPrecedenceAndAssociativity/app.js
@@ -1,3 +1,7 @@
-
-var a = 4 + 3 * 5;
+/**
+ * chart in operator-precedence-in-javascript.pdf and see what order the operator
+ * execution will happen in this case multiplication has a precedence of 14 and
+ * addition has a precedence of 13, so the multiplication will be done first than
+ */
+var a = 3 + 4 * 5;
 console.log(a);
