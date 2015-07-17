6ffc8e046fd9e41e0b66484ce9d8140816342bb5
sosana <thesosana@gmail.com>
2015-07-17 18:54:41 +0000

    working with objects and dot

---
+++ javascript-Understanding-the-Weird-Parts/section4/d1-objects-and-the-dot/Starter/app.js
@@ -0,0 +1,10 @@
+var person = new Object();
+
+person["firstname"] = "Jon";
+person["lastname"] = "Doe";
+
+var firstNameProperty = "firstname";
+
+console.log(person);
+console.log(person[firstNameProperty]);
+console.log(person.firstname);
