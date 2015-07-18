c2d6beb40b6d679e25685e6a670ceddd41a8d785
sosana <thesosana@gmail.com>
2015-07-18 23:36:20 +0000

    creating object containers for namespacing

--- javascript-Understanding-the-Weird-Parts/section4/d3-faking-namespaces/Starter/app.js
+++ javascript-Understanding-the-Weird-Parts/section4/d3-faking-namespaces/Starter/app.js
@@ -1,4 +1,25 @@
+/**
+ * creating fake namespaces by placing the same greet in different objects as namespace containers
+ * so we don't over write variables and have namespace collisions.
+ * you will see this alot in librares and frameworks
+ * These are refer'd to as "container objects" for namespacing and avoiding global namespacing 
+ * collisions that someone else might have written
+ **/
 var greet = 'Hello!';
 var greet = 'Hola!'; 
 
 console.log(greet);
+
+// using these objects as containers so we don't collide with other things in the global namespace
+var english = {
+  greetings: {
+      basic: 'Hello!'
+  }
+};
+
+var spanish = {};
+spanish.greet = 'Hola!';
+
+console.log(english);
+console.log(english.greetings.basic);
+
