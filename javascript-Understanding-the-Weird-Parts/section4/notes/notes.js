8ad441c7158c24f1cc2f524775c63814b84ee15d
sosana <thesosana@gmail.com>
2015-07-18 22:13:36 +0000

    adding notes on objects

--- javascript-Understanding-the-Weird-Parts/section4/notes/notes.js
+++ javascript-Understanding-the-Weird-Parts/section4/notes/notes.js
@@ -1,5 +1,25 @@
 /**
  *  
- * add notes here
+ * Objects And Functions
+ * -  objects are collections of name value pairs. A set of values that are given names
+ * -  an object can have properties and methods. The following could be sitting off of 
+ *    an object:
+ *      - Primitive Property: This is called a property (primitive types) ex: boolean, 
+ *        number, or string
+ *      - Object "property": Can have another object connected to it (as child so to speak)
+ *        this is also considered a property. Remember we said the value could be another 
+ *        name value pair collection or that is another object
+ *      - Function "method": objects can contain functions. In these cases the function is 
+ *        calling a mehthod when its sitting on a object, its connected to a object
+ * -  objects sit in memory and the core object will have some sort of address in your 
+ *    computers memory. It will have references to these different properties and methods
+ *    which are also sitting in your computers memory. The object has references address in
+ *    space, a spot, where these different methods and properties live
+ * -  Think about an object as sitting in memory and than having references to other things 
+ *    sitting memory that are connected it. It knows where its different properites and 
+ *    methods are: that is primitives, objects, and functions that make up the object
+ * 
+ * Objects And The Dot:
+ * - 
  * 
  */
