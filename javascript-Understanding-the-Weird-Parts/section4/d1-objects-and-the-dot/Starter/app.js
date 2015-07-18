9eca992c7cbe2d8e2d4aaab2abc8c70d8169a285
sosana <thesosana@gmail.com>
2015-07-18 22:28:34 +0000

    creating objects with the dot operator

--- javascript-Understanding-the-Weird-Parts/section4/d1-objects-and-the-dot/Starter/app.js
+++ javascript-Understanding-the-Weird-Parts/section4/d1-objects-and-the-dot/Starter/app.js
@@ -1,5 +1,10 @@
+/**
+ * there are better ways to create objects but we will use the new Object() syntax 
+ * that will sit in memory. We'll add property and methods
+ */
 var person = new Object();
-
+// adding properties, The [] on person[] is called the computed member access operator
+// this is one way to set properties
 person["firstname"] = "Jon";
 person["lastname"] = "Doe";
 
@@ -7,4 +12,17 @@ var firstNameProperty = "firstname";
 
 console.log(person);
 console.log(person[firstNameProperty]);
+// using the dot operator, the dot is a function and it takes two parameters
 console.log(person.firstname);
+console.log(person.lastname);
+// creating a new method on person
+person.address = new Object();
+// creating a property street
+person.address.street = "111 Main St.";
+person.address.city = "New York";
+person.address.state = "NY";
+
+console.log(person.address.street);
+console.log(person.address.city);
+// this is doing the same thing as the dot operator
+console.log(person["address"]["state"]);
