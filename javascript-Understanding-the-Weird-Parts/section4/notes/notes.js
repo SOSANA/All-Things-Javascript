b12452332937469161d96bf743f984bdbbc37ddb
sosana <thesosana@gmail.com>
2015-07-19 19:06:23 -0600

    updating notes on functions

--- javascript-Understanding-the-Weird-Parts/section4/notes/notes.js
+++ javascript-Understanding-the-Weird-Parts/section4/notes/notes.js
@@ -1,5 +1,32 @@
 /**
- * Objects And Functions
+ * First Class Functions:
+ * 	- everything you can do with other types such as objects, strings, numbers, and booleans
+ * 		you can do with functions
+ * 	- assign variables to have a value thats a function
+ *  - pass functions around as parameters to other functions
+ *  - create functions on the fly with a kind of literal syntax
+ *
+ * Functions:
+ * 	- functions are objects
+ * 	- it resides in memory
+ * 	- its a special type of a object.
+ * 	- It has all the features of a normal object and has other special properties
+ * 	- you can attach properties and methods to a function
+ * 	- you can attach primitive types, Objects (name value pairs), and functions
+ * 	- has hidden special properties, two of the important ones:
+ * 		- name: optional, can be anonymous
+ * 		- code: where the actual lines of code that you have written sit, the code that you
+ * 						write gets placed into a special property of the function object. The code that
+ * 						you write is one of those properties you are adding on to it. Whats special is
+ * 						that its invocable, meaning you can say run this code sitting on this property
+ * 						and that is when the entire excution context creation happens.
+ *  - you have to think of a function as an object who's code happens to be one of the
+ *  	properties of that object. There are other things that the function can have attached to
+ *  	it, and other things the funciton can do, it can be moved around, copied, and given to
+ *  	other elements or other areas of your code. Just like any objects, value, string and
+ *  	number.
+ *
+ * Objects And Functions:
  *  - objects are collections of name value pairs. A set of values that are given names
  *  - an object can have properties and methods. The following could be sitting off or
  *    connected to an object:
