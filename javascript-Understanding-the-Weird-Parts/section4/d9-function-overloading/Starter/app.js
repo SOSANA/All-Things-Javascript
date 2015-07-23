4bc919661f064b1b10ca61bffe92318e65570e53
sosana <thesosana@gmail.com>
2015-07-23 19:04:11 +0000

    working with pattern for reducing overloading funtions

--- javascript-Understanding-the-Weird-Parts/section4/d9-function-overloading/Starter/app.js
+++ javascript-Understanding-the-Weird-Parts/section4/d9-function-overloading/Starter/app.js
@@ -1,3 +1,35 @@
+/**
+ * example of function overloading
+ */
+
+// one pattern
 function greet(firstname, lastname, language) {
+  language = language || 'en';
+  
+  if (language === 'en') {
+    console.log('Hello ' + firstname + ' ' + lastname);
+  }
+  
+   if (language === 'es') {
+    console.log('Hola ' + firstname + ' ' + lastname);
+  }
         
 }
+
+greet('Jon', 'Doe', 'en');
+greet('Jon', 'Doe', 'es');
+
+// this is just one pattern and a simple one
+// this passes for you your intended parameter values rather than
+// checking and setting the value as shown above 
+function greetEnglish(firstname, lastname) {
+  greet(firstname, lastname, 'en');
+}
+
+function greetSpanish(firstname, lastname) {
+  greet(firstname, lastname, 'es');
+}
+
+// this also helps distinguish between the two
+greetEnglish('Jon', 'Doe');
+greetSpanish('Jon', 'Doe');
