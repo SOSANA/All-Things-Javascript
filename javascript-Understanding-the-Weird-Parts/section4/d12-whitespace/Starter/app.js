54da6dd15d9ffa13047bd34c3262f88c29ce3f6d
sosana <thesosana@gmail.com>
2015-07-24 18:58:28 +0000

    showing the use of the very liberal whitespacing that is allowed in JS

---
+++ javascript-Understanding-the-Weird-Parts/section4/d12-whitespace/Starter/app.js
@@ -0,0 +1,15 @@
+var 
+  // syntax parser is very liberal regarding whitespacing!
+  firstname, 
+  
+  // syntax parser is very liberal regarding whitespacing!
+  lastname, 
+  language;
+
+var person = {
+  // syntax parser is very liberal regarding whitespacing!
+  firstname: 'Joey',
+  lastname: 'Fatone'
+}
+// another example syntax parser is very liberal regarding whitespacing!
+console.log(person);
