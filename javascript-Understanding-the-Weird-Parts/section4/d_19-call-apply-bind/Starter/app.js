f8e9e2669d11c8850b4d618c2ed1863323cb117a
sosana <thesosana@gmail.com>
2015-07-31 18:58:29 +0000

    updating code to show uses of call, apply, and bind methods

--- javascript-Understanding-the-Weird-Parts/section4/d_19-call-apply-bind/Starter/app.js
+++ javascript-Understanding-the-Weird-Parts/section4/d_19-call-apply-bind/Starter/app.js
@@ -34,4 +34,31 @@ logPersonName('en');
 
 // the call method also lets you decide what the 'this' variable will be and
 // lets you control what the 'this' variable will be
-logName.call(person);
+logName.call(person, 'en', 'es');
+
+// same as call but wants an array of parameters
+logName.apply(person, ['en', 'es']);
+
+// here's our function expression that executes on the fly
+(function(lang1, lang2) {
+  
+  console.log('Logged: ' + this.getFullname());
+  console.log('Arugments: ' + lang1 + ' ' + lang2);
+  console.log('-----------');
+// another way to apply the "apply" method to a funcntion expression
+}.apply(person, ['es', 'en']));
+
+// function borrowing
+var person2 = {
+  firstname: 'Jane',
+  lastname: 'Doe'
+}
+// setting the "this" variable to person2 object
+// you can grab methods from other objects and use them long as they have similar names so the function works
+console.log(person.getFullname.apply(person2));
+
+// function currying
+
+ 
+ 
+ 
