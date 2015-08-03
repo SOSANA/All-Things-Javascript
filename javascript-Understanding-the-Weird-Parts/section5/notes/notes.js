3fc3b1535d113c40c622aebaeb5fe5cb6dd4517e
sosana <thesosana@gmail.com>
2015-08-03 18:32:18 +0000

    adding notes about Classical Vs Prototypal Inheritance

--- javascript-Understanding-the-Weird-Parts/section5/notes/notes.js
+++ javascript-Understanding-the-Weird-Parts/section5/notes/notes.js
@@ -1,5 +1,18 @@
 /**
- *  
- * add notes here
- * 
+ * Classical Vs Prototypal Inheritance
+ *  - Inheritance
+ *    - one object gets access to the properites and methods of another object.
+ *  - Classical Inheritance
+ *    - commonly use in classical programming such as java, c++, php, etc
+ *    - considered verbose, uses friend, protected, private, interface
+ *  - Protypal Inheritance
+ *    - Simple, flexible, extensible and easy to understand
+ *    - two patterns of prototypal inheritance
+ *        - prototypal pattern of prototypal inheritance
+ *        - the constructor pattern of prototypal inheritance
+ *    - there are two ways of creating objects using prototypal inheritance
+ *        - create a brand new object.
+ *        - clone an existing object and extend it
+ *  - good explanation found here: 
+ *    http://stackoverflow.com/questions/2800964/benefits-of-prototypal-inheritance-over-classical
  */
