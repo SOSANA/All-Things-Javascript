556cea1782b17957d713a17206e1a03e21cdbcc2
sosana <thesosana@gmail.com>
2015-08-04 18:43:12 +0000

    adding notes on function constructors

--- javascript-Understanding-the-Weird-Parts/section6/notes/notes.js
+++ javascript-Understanding-the-Weird-Parts/section6/notes/notes.js
@@ -1,5 +1,6 @@
 /**
- *  
- * add notes here
- * 
+ * Function Constructors:
+ *  - a normal function that is used to construct objects
+ *  - the 'this' variable points to a new empty object, and that object is returned from the
+ *    function automatically
  */
