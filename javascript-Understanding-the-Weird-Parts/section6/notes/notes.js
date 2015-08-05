1412603a51595bc59a325cda22ed623ebb9e21e4
sosana <thesosana@gmail.com>
2015-08-05 18:27:08 +0000

    adding notes about prototypes

--- javascript-Understanding-the-Weird-Parts/section6/notes/notes.js
+++ javascript-Understanding-the-Weird-Parts/section6/notes/notes.js
@@ -3,4 +3,12 @@
  *  - a normal function that is used to construct objects
  *  - the 'this' variable points to a new empty object, and that object is returned from the
  *    function automatically
+ * 
+ * Prototype:
+ *  - all functions have a property called prototype that starts out as an empty object
+ *  - prototype property is used only by the "new" operator
+ *  - allows access to all objects just once, rather than every object taking up memory space
+ *    and getting a copy for every object ex: if you have a 1000 Person Objects with getFullName 
+ *    methods you will have a 1000 getFullName methods taking up memory space rather than having
+ *    a prototype you will only have need one method
  */
