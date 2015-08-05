d03c14099e39e4946db7806eca0e39760dc163ce
sosana <thesosana@gmail.com>
2015-08-05 18:32:42 +0000

    updating notes on prototypes

--- javascript-Understanding-the-Weird-Parts/section6/notes/notes.js
+++ javascript-Understanding-the-Weird-Parts/section6/notes/notes.js
@@ -6,7 +6,9 @@
  * 
  * Prototype:
  *  - all functions have a property called prototype that starts out as an empty object
- *  - prototype property is used only by the "new" operator
+ *  - always use captial letter ex: function Person(firstname, lastname) {};
+ *  - prototype property is used only by the "new" operator. Make sure to always use "new"
+ *    assigning prototypes to objects as it will come back undefined
  *  - allows access to all objects just once, rather than every object taking up memory space
  *    and getting a copy for every object ex: if you have a 1000 Person Objects with getFullName 
  *    methods you will have a 1000 getFullName methods taking up memory space rather than having
