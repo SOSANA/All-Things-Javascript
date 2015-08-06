e46ab6ec50d1b032755f141ff15cd47af0984151
sosana <thesosana@gmail.com>
2015-08-06 18:47:37 +0000

    showing example as to why you shouldn't use for..in with arrays

--- javascript-Understanding-the-Weird-Parts/section6/F4_BuiltInFunctionConstructors/Starter/app.js
+++ javascript-Understanding-the-Weird-Parts/section6/F4_BuiltInFunctionConstructors/Starter/app.js
@@ -1,5 +1,7 @@
-// the primitive string was automatically convert to a string object
-// all strings every where have access to this string prototype
+/**
+ * the primitive string was automatically convert to a string object
+ * all strings every where have access to this string prototype
+ */
 String.prototype.isLengthGreaterThan = function(limit) {
   return this.length > limit;
 }
@@ -13,3 +15,18 @@ Number.prototype.isPositive = function() {
 var a = new Number(3);
 
 console.log(a.isPositive());
+
+/**
+ * Dangerous Aside: Arrays and for..in showing problems you could have
+ * Be carefull! Don't use for..in with arrays, use a standard for loop
+ */
+// this could be from a library or something you created
+Array.prototype.myCustomFeature = 'cool!';
+
+
+var arr = ['John', 'Jane', 'Jim'];
+
+for (var prop in arr) {
+  console.log(prop + ': ' + arr[prop]); // 0, 1, 2 is the property names
+}
+
