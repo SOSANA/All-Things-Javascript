866517e1e35df85ffb7b2f9cd1c9138729d34e49
sosana <thesosana@gmail.com>
2015-08-06 18:28:36 +0000

    working with built-in function constructors

---
+++ javascript-Understanding-the-Weird-Parts/section6/F4_BuiltInFunctionConstructors/Starter/app.js
@@ -0,0 +1,11 @@
+// the primitive string was automatically convert to a string object
+// all strings every where have access to this string prototype
+String.prototype.isLengthGreaterThan = function(limit) {
+  return this.length > limit;
+}
+
+console.log("John".isLengthGreaterThan(3));
+
+Number.prototype.isPositive = function() {
+  return this > 0;
+}
