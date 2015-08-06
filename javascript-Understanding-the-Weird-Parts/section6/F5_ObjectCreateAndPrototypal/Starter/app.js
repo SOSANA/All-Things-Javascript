c5b9572b9c83b55eeb9339653e287cf1f1b7f238
sosana <thesosana@gmail.com>
2015-08-06 19:05:20 +0000

    creating pure pure prototypal inheritance with Object.create()

---
+++ javascript-Understanding-the-Weird-Parts/section6/F5_ObjectCreateAndPrototypal/Starter/app.js
@@ -0,0 +1,33 @@
+/**
+ * Another way to create objects not using function constructors(was introduced to mimic other languages similar to class objects)
+ * This all about building objects with prototypes. These examples show pure prototypal inheritance
+ */
+
+// polyfill
+// if we are using an older browser engine that does not have Object.create
+// this whole state will be skipped if the Object.create exists
+if (!Object.create) {
+  Object.create = function (o) {
+    if (arguments.length > 1) {
+      throw new Error('Object.create implementation'
+      + ' only accepts the first parameter.');
+    }
+    function F() {}
+    F.prototype = o;
+    return new F();
+  };
+}
+
+var person = {
+  firstname: 'Default',
+  lastname: 'Default',
+  greet: function() {
+    return 'Hi ' + this.firstname;
+  }
+}
+
+var john = Object.create(person);
+john.firstname = 'John';
+john.lastname = 'Doe';
+console.log(john);
+console.log(john.greet());
