1ed8421d3e4c7594f5db9f242d7bb79ed5e23abc
sosana <thesosana@gmail.com>
2015-08-08 19:33:58 +0000

    updating

--- javascript-Understanding-the-Weird-Parts/section7/notes/notes.js
+++ javascript-Understanding-the-Weird-Parts/section7/notes/notes.js
@@ -1,5 +1,5 @@
 /**
- *  
- * add notes here
- * 
+ * Strict Mode:
+ * - Read more about strict mode on the MDN (Mozilla Developer Network): 
+ *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
  */
