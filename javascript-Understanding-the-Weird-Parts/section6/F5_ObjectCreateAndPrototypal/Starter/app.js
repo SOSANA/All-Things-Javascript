964f76fd703145c848f31070c4d3e8cd9a489e3b
SOSANA <sosana@gmail.com>
2015-08-09 12:41:55 -0600

    outputting object proto

--- javascript-Understanding-the-Weird-Parts/section6/F5_ObjectCreateAndPrototypal/Starter/app.js
+++ javascript-Understanding-the-Weird-Parts/section6/F5_ObjectCreateAndPrototypal/Starter/app.js
@@ -9,8 +9,7 @@
 if (!Object.create) {
   Object.create = function (o) {
     if (arguments.length > 1) {
-      throw new Error('Object.create implementation'
-      + ' only accepts the first parameter.');
+      throw new Error('Object.create implementation' + ' only accepts the first parameter.');
     }
     function F() {}
     F.prototype = o;
@@ -24,10 +23,11 @@ var person = {
   greet: function() {
     return 'Hi ' + this.firstname;
   }
-}
+};
 
 var john = Object.create(person);
 john.firstname = 'John';
 john.lastname = 'Doe';
 console.log(john);
 console.log(john.greet());
+console.log(john.__proto__);
