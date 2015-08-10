927b4a95511480660d138745f8a13e9cbf6aafa7
sosana <thesosana@gmail.com>
2015-08-10 18:28:17 +0000

    updating notes on Initialization

--- javascript-Understanding-the-Weird-Parts/section7/notes/notes.js
+++ javascript-Understanding-the-Weird-Parts/section7/notes/notes.js
@@ -1,5 +1,10 @@
 /**
+ * Initialization:
+ *  - clean and useful way to initialize data and also nice for prototyping for UI 
+ *    and testing prior to implementing the full software and database etc
+ *    ex: /H1_Initialization
+ * 
  * Strict Mode:
- * - Read more about strict mode on the MDN (Mozilla Developer Network):
- *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+ *  - Read more about strict mode on the MDN (Mozilla Developer Network):
+ *    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
  */
