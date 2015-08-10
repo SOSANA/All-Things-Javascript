fa36028c48d16c11a27bbecc445319dadb90c739
SOSANA <sosana@gmail.com>
2015-08-09 21:54:51 -0600

    updating comments on using babel.js cli

--- javascript-Understanding-the-Weird-Parts/section6/es6AndClasses/preapp.js
+++ javascript-Understanding-the-Weird-Parts/section6/es6AndClasses/preapp.js
@@ -1,6 +1,16 @@
 /**
  * working with es6 and classes, a new way to set prototype objects
- *
+ * Using Babel.JS src: https://babeljs.io/docs/usage/cli/
+ * Using npm you can install Babel globally, making it available from the command line.
+ * 	$ npm install --global babel
+ * Compile Files
+ * Compile the file script.js and output to stdout.
+ * 	$ babel script.js
+ * 	# output...
+ * If you would like to output to a file you may use --out-file or -o.
+ * 	$ babel preapp.js --out-file app.js
+ * To compile a file every time that you change it, use the --watch or -w option:
+ * 	$ babel preapp.js --watch --out-file app.js
  */
 class Person {
   constructor(firstname, lastname) {
