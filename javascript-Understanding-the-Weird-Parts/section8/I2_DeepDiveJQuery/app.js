ca264822f259b8fd9fce61c4ad9b9ce1f22656b7
sosana <thesosana@gmail.com>
2015-08-12 18:47:53 +0000

    working with method chaining and deep dive into reading src code

--- javascript-Understanding-the-Weird-Parts/section8/I2_DeepDiveJQuery/app.js
+++ javascript-Understanding-the-Weird-Parts/section8/I2_DeepDiveJQuery/app.js
@@ -1,2 +1,8 @@
-var q = $("ul.people li");
+/**
+ * jQuery is just a giant library full of method and properties, look in console.log __proto__ or src
+ * refer to notes in section 6 on method chaining
+ */
+
+// this is called method chaining
+var q = $("ul.people").addClass("newClass").removeClass("people");
 console.log(q);
