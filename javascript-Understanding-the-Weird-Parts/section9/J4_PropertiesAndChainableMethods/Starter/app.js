4cba0534eb081f07af54418f5c2f65a1e1014f44
sosana <thesosana@gmail.com>
2015-08-14 18:53:12 +0000

    testing updated properties and prototype methods

--- javascript-Understanding-the-Weird-Parts/section9/J4_PropertiesAndChainableMethods/Starter/app.js
+++ javascript-Understanding-the-Weird-Parts/section9/J4_PropertiesAndChainableMethods/Starter/app.js
@@ -1,2 +1,8 @@
 var g = G$('John', 'Doe');
-console.log(g);
+// chaining some methods
+g.greet().greet(true);
+console.log('-----------');
+// chaining some methods
+g.greet().setLang('es').greet(true);
+console.log('-----------');
+g.setLang('fr').greet(true); // should show error as french is not supported language
