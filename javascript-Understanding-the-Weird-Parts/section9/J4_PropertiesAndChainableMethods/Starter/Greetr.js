42fe9f92f563aec0c9a0e44d939aadba84bfbe3e
sosana <thesosana@gmail.com>
2015-08-14 18:52:24 +0000

    updating properties and prototype methods

--- javascript-Understanding-the-Weird-Parts/section9/J4_PropertiesAndChainableMethods/Starter/Greetr.js
+++ javascript-Understanding-the-Weird-Parts/section9/J4_PropertiesAndChainableMethods/Starter/Greetr.js
@@ -9,9 +9,84 @@
     return new Greetr.init(firstName, lastName, language);
   }
   
-  // empty object for now, here is were we'll put any methods that we 
-  // want to use inside our object that is returned from Greetr
-  Greetr.prototype = {};
+  // thanks to closures we have access to its lexical environment inside this
+  // function
+  var supportedLangs = ['en', 'es'];
+  
+  var greetings = {
+    en: 'Hello',
+    es: 'Hola'
+  };
+  
+  var formalGreetings = {
+    en: 'Greetings',
+    es: 'Saludos'
+  };
+  
+  var logMessages = {
+    en: 'Logged in',
+    es: 'Inicio sesion'
+  };
+  
+  // here is were we'll put any methods that we want to use inside our object that 
+  // is returned from Greetr
+  Greetr.prototype = {
+    
+    fullName: function() {
+      return this.firstName + ' ' + this.lastName;
+    },
+    
+    validate: function() {
+      // looking in the supportedLangs array to see if it supports given lang 
+      if (supportedLangs.indexOf(this.language) === -1) {
+        throw "Invalid language";
+      }
+    },
+    
+    greeting: function() {
+      return greetings[this.language] + ' ' + this.firstName + '!';
+    },
+    
+    formalGreeting: function() {
+      return formalGreetings[this.language] + ', ' + this.fullName();
+    },
+    
+    // chaining methods
+    greet: function(formal) {
+      var msg;
+      // if undefined or null it will be coerced to 'false'
+      if (formal) {
+        msg = this.formalGreeting();
+      }
+      else {
+        msg = this.greeting();
+      }
+      
+      if (console) {
+        console.log(msg);
+      }
+      
+      // 'this' refers to the calling object at execution time
+      // makes the method chainable
+      return this;
+    },
+      
+    log: function() {
+      if (console) {
+        console.log(logMessages[this.language] + ': ' + this.fullName());
+      }
+      
+      return this;
+    },
+    
+    setLang: function(lang) {
+      this.language = lang;
+      
+      this.validate();
+      
+      return this;
+    }
+  };
   
   // a function constructor that builds properties sets its value if you pass 
   // something into your object otherwise set some defaults
