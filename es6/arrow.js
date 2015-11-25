0fafca409418f70061fc5ef529c9d9662d141cc4
sosana <thesosana@gmail.com>
2015-11-25 14:50:32 -0700

    updating examples

--- es6/arrow.js
+++ es6/arrow.js
@@ -12,3 +12,42 @@
 
  // ES5
  [1, 2, 3].map(function(n) { return n * 2; }); // [2, 4, 6]
+
+ /**
+  * Every function expression above creates its own this scope. Without binding
+  * this we would not be able to call this.setState in the example above, because
+  * this would have been undefined.
+  *
+  * Alternatively, we could have assigned this to a variable, e.g. var self = this
+  * and then used self.setState instead of this.setState inside the closures to
+  * get around this classic JavaScript problem.
+  */
+
+  // ES5
+  $.ajax({ type: 'POST', url: '/api/characters', data: { name: name, gender: gender } })
+   .done(function(data) {
+     this.setState({ helpBlock: data.message });
+   }.bind(this))
+   .fail(function(jqXhr) {
+     this.setState({ helpBlock: jqXhr.responseJSON.message });
+   }.bind(this))
+   .always(function() {
+     this.setState({ name: '', gender: '' });
+   }.bind(this));
+
+   /**
+    * n equivalent ES6 code using fat arrow functions which preserve the original
+    * this value:
+    */
+
+   // ES6
+    $.ajax({ type: 'POST', url: '/api/characters', data: { name: name, gender: gender } })
+      .done((data) => {
+        this.setState({ helpBlock: data.message });
+      })
+      .fail((jqXhr) => {
+        this.setState({ helpBlock: jqXhr.responseJSON.message });
+      })
+      .always(() => {
+        this.setState({ name: '', gender: '' });
+      });
