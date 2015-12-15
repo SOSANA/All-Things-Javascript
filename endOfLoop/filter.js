3d26f31b3c2be87f18a36fe49a42495c05863288
sosana <thesosana@gmail.com>
2015-12-15 14:05:59 -0700

    creating example for array filter method

---
+++ endOfLoop/filter.js
@@ -0,0 +1,18 @@
+/**
+ * - using filter method, to filter out items in an array and only returning
+ *   items that match a certain criteria
+ */
+function getStocksOver(stocks, minPrice) {
+  return stocks.filter(function(stock) {
+    return stock.price <= minPrice;
+  })
+}
+
+var expensiveStocks = getStocksOver([
+  { symbol: "XFX", price: 240.22, volume: 23432 },
+  { symbol: "TNZ", price: 332.19, volume: 234 },
+  { symbol: "JXJ", price: 120.22, volume: 5323 },
+],
+150.00);
+
+console.log(JSON.stringify(expensiveStocks));
