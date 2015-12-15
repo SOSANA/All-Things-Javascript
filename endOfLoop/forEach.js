4346162b7d8ad1965fa092d830f19c57ed056641
sosana <thesosana@gmail.com>
2015-12-15 13:49:45 -0700

    updating notes on using the forEach method

--- endOfLoop/forEach.js
+++ endOfLoop/forEach.js
@@ -1,19 +1,24 @@
 /**
  * - using foreach method
+ * - useful if you want to do something to every item in the array
  * - runs asynchronously
  * - accepts a closure and invokes that closure for every item in the array and passes
  *   in that item as an argument to the closure
  * - really convenient if you want to do something to every item in an array
  */
 
+ // 'getStockSymbols' function, job is to get an array of stocks, pull out the symbol from
+ // each one of those stocks and put all the symbols in an array and then return it
 function getStockSymbols(stocks) {
   var symbols = [];
-  
+
   // this works exactly like our for loop
   stocks.forEach(function(stock) {
     symbols.push(stock.symbol);
+    //symbols.push(stock.symbol, stock.volume);
+    //symbols.push(stock.volume);
   })
-      
+
   return symbols;
 }
 
