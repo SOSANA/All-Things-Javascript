b327a5404cfa185feb7e2a3dbed9b370292a6826
sosana <thesosana@gmail.com>
2016-02-21 13:11:11 -0700

    updating notes

--- javascript-Understanding-the-Weird-Parts/notes/section5notes.js
+++ javascript-Understanding-the-Weird-Parts/notes/section5notes.js
@@ -13,13 +13,13 @@
  *    - there are two ways of creating objects using prototypal inheritance
  *        - create a brand new object.
  *        - clone an existing object and extend it
- *  - good explanation found here: 
+ *  - good explanation found here:
  *    http://stackoverflow.com/questions/2800964/benefits-of-prototypal-inheritance-over-classical
- * 
+ *
  * Prototype:
  *  - all functions and arrays have prototype pointing to an object with those function methods
  *    -  see line 26 ex: /E2_UnderstandingThePrototype
- * 
+ *
  * Reflection:
  *  - an object can look at itself, listing, and changing its properties and methods
  */
