34652b5af090ef6bc44098eb987a372a99d32ff0
sosana <thesosana@gmail.com>
2016-02-28 16:00:19 -0700

    updating directory

--- javascript-Understanding-the-Weird-Parts/section3/C8_BooleansExistence/Finished/app.js
+++
@@ -1,7 +0,0 @@
-var a;
-
-// goes to internet and looks for a value
-
-if (a) {
-  console.log('Something is there.');  
-}
--- javascript-Understanding-the-Weird-Parts/section3/C9_DefaultValues/Finished/app.js
+++
@@ -1,7 +0,0 @@
-function greet(name) {
-    name = name || '<Your name here>';
-    console.log('Hello ' + name);    
-}
-
-greet('Tony');
-greet();
