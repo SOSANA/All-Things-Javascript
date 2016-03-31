898cbb881f7a640d1a7fa54d50a02ea1e54a050c
sosana <thesosana@gmail.com>
2016-03-30 18:10:33 -0600

    updating

--- eloquentJavaScript/ch02/chessBoard.js
+++ eloquentJavaScript/ch02/chessBoard.js
@@ -1,3 +1,19 @@
 /**
+ * Write a program that creates a string that represents an 8×8 grid, using newline
+ * characters to separate lines. At each position of the grid there is either a space
+ * or a “#” character. The characters should form a chess board. Passing this string to
+ * console.log should show something like this:
  *
+ *  # # # #
+ * # # # #
+ *  # # # #
+ * # # # #
+ *  # # # #
+ * # # # #
+ *  # # # #
+ * # # # #
+ *
+ * When you have a program that generates this pattern, define a variable size = 8 and
+ * change the program so that it works for any size , outputting a grid of the given width
+ * and height.
  */
