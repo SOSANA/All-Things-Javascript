6cef6dd0359e0434168603a368a83a8c3657da37
sosana <thesosana@gmail.com>
2016-04-01 11:01:13 -0600

    adding math power function with a base number and an exponent as arugments

---
+++ jsPlayground/math/power.js
@@ -0,0 +1,16 @@
+/**
+ * Powers and Exponents. A power is the product of multiplying a number by itself. Usually, a
+ * power is represented with a base number and an exponent. The base number tells what number
+ * is being multiplied.
+ */
+
+ var power = function (base, exponent) {
+   var result = 1;
+
+   for (var count = 0; count < exponent; count++) {
+     result *= base;
+   }
+   return result;
+ };
+
+ console.log(power(2,10));
