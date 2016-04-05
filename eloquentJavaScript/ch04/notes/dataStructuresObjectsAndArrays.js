2f71ce6c937170e13b331e4148839d2f6802d91f
sosana <thesosana@gmail.com>
2016-04-05 15:21:06 -0600

    updating ch04 directory

---
+++ eloquentJavaScript/ch04/notes/dataStructuresObjectsAndArrays.js
@@ -0,0 +1,26 @@
+/**
+ * Data sets:
+ *  -
+ * Properties:
+ *  -
+ * Methods:
+ *  -
+ * Objects:
+ *  -
+ * Mutability:
+ *  -
+ * Computing correlation:
+ *  -
+ * Objects as maps:
+ *  -
+ * Further arrayology:
+ *  -
+ * Strings and their properties:
+ *  -
+ * The arguments object:
+ *  -
+ * The Math object:
+ *  -
+ * The global object:
+ *  -
+ */
