add8a0a64fb2da268194aa71435a3256c1c7fcf9
sosana <thesosana@gmail.com>
2016-04-11 15:50:58 -0600

    updating syntax for readability

--- eloquentJavaScript/ch04/weresquirrel/app.js
+++ eloquentJavaScript/ch04/weresquirrel/app.js
@@ -32,18 +32,29 @@ console.log('-------------');
 
 import { JOURNAL } from './jacques_journal';
 
+// check to see if event exists
 function hasEvent(event, entry) {
-  return entry.events.indexOf(event) != -1;
+  return entry.events.indexOf(event) !== -1;
 }
 
 function tableFor(event, journal) {
   var table = [0, 0, 0, 0];
+
   for (var i = 0; i < journal.length; i++) {
-    var entry = journal[i], index = 0;
-    if (hasEvent(event, entry)) index += 1;
-    if (entry.squirrel) index += 2;
+    var entry = journal[i];
+    var index = 0;
+
+    if (hasEvent(event, entry)) {
+      index += 1
+    }
+
+    if (entry.squirrel) {
+      index += 2
+    }
+
     table[index] += 1;
   }
+
   return table;
 }
 
