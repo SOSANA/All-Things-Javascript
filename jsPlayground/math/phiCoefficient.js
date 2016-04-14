42ca95efb4f0f2c60a720ffebece99c33fe96cf5
sosana <thesosana@gmail.com>
2016-04-14 16:25:12 -0600

    updating phi example

--- jsPlayground/math/phiCoefficient.js
+++ jsPlayground/math/phiCoefficient.js
@@ -7,6 +7,14 @@
  *  	were observed.
  *  - ϕ can be computed using the following formula, where n refers to the table:
  *  	ϕ = n11n00 - n10n01 / √ n1•n0•n•1n•0
+ *
+ * 		Ex:
+ * 	  ---------------------------------------------------
+ * 		| No pizza, no squirrel 76 | pizza, no squirrel 9 |
+ * 		|--------------------------|----------------------|
+ * 		| No pizza, squirrel 4     | pizza, squirrel 1    |
+ * 		---------------------------------------------------
+ *
  *  	The notation n 01 indicates the number of measurements where the first variable
  *   	(squirrelness) is false (0) and the second variable (pizza) is true (1). In this
  *    example, n 01 is 9. The value n 1• refers to the sum of all measurements where the
@@ -18,16 +26,74 @@
  *    have influence on the transformations.
  */
 
- /*eslint-disable */
+ /* eslint-disable */
  // We can represent a two-by-two table in JavaScript with a four-element array ([76, 9, 4, 1]).
  // The flat array is simple and makes the expressions that access the table pleasantly short.
  // We’ll interpret the indices to the array as two-bit binary number.
 function phi(table) {
-  return ( table[3] * table[0] - table[2] * table[1]) /
+  return (table[3] * table[0] - table[2] * table[1]) /
     Math.sqrt((table[2] + table[3]) *
       (table[0] + table[1]) *
       (table[1] + table[3]) *
       (table[0] + table[2])
     );
+}
+console.log(phi([ 76 , 9 , 4 , 1 ])); // 0.068599434
+console.log('-------------');
+
+
+/**
+ * The table is trying to record all the different possible outcomes from the if statements.
+ *  - You get 0 if there's no event, and there's no squirrel.
+ *  - You get 1 if there's an event but no squirrel.
+ *  - You get 2 if there's no event, but there is a squirrel.
+ *  - you get 3 if there is an event, and a squirrel.
+ * You then increment one of four different positions in the table variable index based results.
+ * You can basically think of index as keeping track of all possible states.
+ * Since you're iterating over a journal with i, you're trying to keep track of what's happening.
+ *
+ * Note:
+ * - checkout /All-Things-Javascript/eloquentJavaScript/ch04/weresquirrel/
+ */
+
+ var JOURNAL = [
+   {"events":["carrot","exercise","weekend"],"squirrel":false},
+   {"events":["bread","pudding","brushed teeth","weekend","touched tree"],"squirrel":false},
+   {"events":["carrot","nachos","brushed teeth","cycling","weekend"],"squirrel":false},
+   {"events":["brussel sprouts","ice cream","brushed teeth","computer","weekend"],"squirrel":false},
+   {"events":["potatoes","candy","brushed teeth","exercise","weekend","dentist"],"squirrel":false},
+   {"events":["brussel sprouts","pudding","brushed teeth","running","weekend"],"squirrel":false},
+   {"events":["pizza","brushed teeth","computer","work","touched tree"],"squirrel":false},
+   {"events":["bread","beer","brushed teeth","cycling","work"],"squirrel":false},
+   {"events":["cauliflower","brushed teeth","work"],"squirrel":false},
+   {"events":["pizza","brushed teeth","cycling","work"],"squirrel":false},
+ ];
+
+// check to see if event exists
+function hasEvent(event, entry) {
+  return entry.events.indexOf(event) !== -1;
+}
+
+// record all possible outcomes and place values by index
+function tableFor(event, journal) {
+  var table = [0, 0, 0, 0];
+
+  for (var i = 0; i < journal.length; i++) {
+    var entry = journal[i];
+    // You get index 0 if there's no event, and there's no squirrel.
+    var index = 0;
+    // You get index 1 if there's an event but no squirrel.
+    if (hasEvent(event, entry)) {
+      index += 1;
+    }
+
+    if (entry.squirrel) {
+      index += 2;
+    }
+
+    table[index] += 1;
   }
-  console.log(phi([ 76 , 9 , 4 , 1 ])); // → 0.068599434
+  return table;
+}
+console.log(tableFor('pizza', JOURNAL));
+console.log('-------------');
