0cced4578843e4311570d0db7ac8ba2da15a9358
sosana <thesosana@gmail.com>
2016-04-20 16:50:15 -0600

    updating notes on exercises

--- eloquentJavaScript/ch04/01_theSumOfARange.js
+++ eloquentJavaScript/ch04/01_theSumOfARange.js
@@ -1,3 +1,11 @@
 /**
- * add notes here
+ * To compute the sum of a range of numbers: console.log(sum(range(1, 10)));
+ * Write a range function that takes two arguments, start and end, and returns an array containing all
+ * the numbers from start up to (and including) end.
+ * Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run
+ * the previous program and see whether it does indeed return 55.
+ * As a bonus assignment, modify your range function to take an optional third argument that indicates
+ * the “step” value used to build up the array. If no step is given, the array elements go up by increments
+ * of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9].
+ * Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
  */
--- eloquentJavaScript/ch04/02_reversingAnArray.js
+++ eloquentJavaScript/ch04/02_reversingAnArray.js
@@ -1,3 +1,8 @@
 /**
- * add notes here
+ * Arrays have a method reverse, which changes the array by inverting the order in which its elements appear.
+ * For this exercise, write two functions, reverseArray and reverseArrayInPlace.
+ * The first, reverseArray, takes an array as argument and produces a new array that has the same elements in
+ * the inverse order.
+ * The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument
+ * in order to reverse its elements. Neither may use the standard reverse method.
  */
