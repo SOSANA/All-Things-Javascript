ea838996d0868ae0f37a1a093bc893b46aff7feb
sosana <thesosana@gmail.com>
2016-04-21 14:06:19 -0600

    updating example sum of range

--- eloquentJavaScript/ch04/01_theSumOfARange.js
+++ eloquentJavaScript/ch04/01_theSumOfARange.js
@@ -9,3 +9,38 @@
  * of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9].
  * Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
  */
+
+function range(start, end, step){
+  if (step === undefined) {
+    step = 1;
+  }
+
+  var output = [];
+
+  if (step > 0) {
+    for (var i = start; i <= end; i += step) {
+      output.push(i);
+    }
+  } else {
+    for (var j = start; j >= end; j += step) {
+      output.push(j);
+    }
+  }
+
+  return output;
+}
+
+function sum(input) {
+  var total = 0;
+  for (var i = 0; i < input.length; i++) {
+    var value = input[i];
+    total += value;
+  }
+  return total;
+}
+
+console.log(sum(range(1, 10))); // 55
+console.log(range(2, 10, 2)); // [2, 4, 6, 8, 10]
+console.log(range(20, 3, -3)); // [20, 17, 14, 11, 8, 5]
+console.log(sum(range(5, 10))); // 45
+console.log(range(50, 1, -5)); // [50, 45, 40, 35, 30, 25, 20, 15, 10, 5]
