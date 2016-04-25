da3f9932b97eb51dd70d3ac12bfc61946c736a9d
sosana <thesosana@gmail.com>
2016-04-25 15:43:33 -0600

    updating exercise notes

--- eloquentJavaScript/ch04/03_aList.js
+++ eloquentJavaScript/ch04/03_aList.js
@@ -1,3 +1,22 @@
 /**
- * add notes here
+ * Objects, as generic blobs of values, can be used to build all sorts of data structures. A common data structure is the list
+ * (not to be confused with the array). A list is a nested set of objects, with the first object holding a reference to the
+ * second, the second to the third, and so on.
+ * var list = {
+ * 	value: 1,
+ * 	rest: {
+ * 		value: 2,
+ * 		rest: {
+ * 			value: 3,
+ * 			rest: null
+ * 		}
+ * 	}
+ * };
+ * The resulting objects form a chain, like this:
+ * value: 1
+ * rest:   → value: 2
+ *           rest:   → value: 3
+ *           					 rest: null
  */
+
+/* eslint-disable */
--- eloquentJavaScript/ch04/04_deepComparison.js
+++ eloquentJavaScript/ch04/04_deepComparison.js
@@ -1,3 +1,13 @@
 /**
- * add notes here
+ * The == operator compares objects by identity. But sometimes, you would prefer to compare the values of their actual
+ * properties.
+ *
+ * Write a function, deepEqual, that takes two values and returns true only if they are the same value or are objects
+ * with the same properties whose values are also equal when compared with a recursive call to deepEqual.
+ *
+ * To find out whether to compare two things by identity (use the === operator for that) or by looking at their properties,
+ * you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. But you have
+ * to take one silly exception into account: by a historical accident, typeof null also produces "object".
  */
+
+/* eslint-disable */
