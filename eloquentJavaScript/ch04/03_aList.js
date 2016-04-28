9bc46724f90bd2190ed03a2c73e64f2206547252
sosana <thesosana@gmail.com>
2016-04-28 14:13:47 -0600

    udating notes on creating lists(objects) with functions

--- eloquentJavaScript/ch04/03_aList.js
+++ eloquentJavaScript/ch04/03_aList.js
@@ -17,6 +17,18 @@
  * rest:   → value: 2
  *           rest:   → value: 3
  *           					 rest: null
+ * A nice thing about lists is that they can share parts of their structure. For example, if I create two new values
+ * {value: 0, rest: list} and {value: -1, rest: list} (with list referring to the variable defined earlier), they are both
+ * independent lists, but they share the structure that makes up their last three elements. In addition, the original list is
+ * also still a valid three-element list.
+ *
+ * Write a function arrayToList that builds up a data structure like the previous one 
+ * when given [1, 2, 3] as argument, and write a listToArray function that produces an array from a list. Also write the helper
+ * functions prepend , which takes an element and a list and creates a new list that adds the element to the front of the input
+ * list, and nth , which takes a list and a number and returns the element at the given position in the list, or undefined when
+ * there is no such element.
+ *
+ * If you haven’t already, also write a recursive version of nth .
  */
 
 /* eslint-disable */
