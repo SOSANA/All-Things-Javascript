93cee3beb3cd592bed93303c7c7db5836b699ede
sosana <thesosana@gmail.com>
2016-04-29 17:04:02 -0600

    updating type checking of object comparison with recursion

--- eloquentJavaScript/ch04/04_deepComparison.js
+++ eloquentJavaScript/ch04/04_deepComparison.js
@@ -68,4 +68,4 @@ var obj = { here: { is: "an" }, object: 2};
 
 console.log(deepEqual(obj, obj)); // true
 console.log(deepEqual(obj, { here: 1, object: 2 })); // false
-console.log(deepEqual(obj, { here: {is: "an"}, object: 2 })); // true
+console.log(deepEqual(obj, { here: { is: "an" }, object: 2 })); // true
