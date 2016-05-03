843f001de28c7ac3ee1a81146fea7ce30b7622c3
sosana <thesosana@gmail.com>
2016-05-03 13:23:55 -0600

    updating notes on higher order function

--- eloquentJavaScript/ch05/notes/notes.js
+++ eloquentJavaScript/ch05/notes/notes.js
@@ -1,5 +1,34 @@
 /**
+ * Abstraction:
+ * 	-
  *
- * add notes here
+ * Abstracting array traversal:
+ * 	-
  *
+ * Higher-order functions:
+ * 	-
+ *
+ * Passing along arguments:
+ * 	-
+ *
+ * JSON:
+ * 	-
+ *
+ * Filtering an array:
+ * 	-
+ *
+ * Transforming with map:
+ * 	-
+ *
+ * Summarizing with reduce:
+ * 	-
+ *
+ * Composability:
+ * 	-
+ *
+ * The cost:
+ * 	-
+ *
+ * Binding:
+ * 	-
  */
