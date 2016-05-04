a7f9979e3016555504c7dcb50ace36d81584d432
sosana <thesosana@gmail.com>
2016-05-04 12:26:52 -0600

    updating exercises on higher order functions

--- eloquentJavaScript/ch05/05_1_flattening.js
+++ eloquentJavaScript/ch05/05_1_flattening.js
@@ -1,3 +1,6 @@
 /**
- * add notes here
+ * Flattening:
+ * 	- Use the reduce method in combination with the concat method to “flatten” an array of arrays
+ * 		into a single array that has all the elements of the input arrays
  */
+var array = [[1, 2, 3], [4, 5], [6]];
--- eloquentJavaScript/ch05/05_2_mother-child_age_difference.js
+++ eloquentJavaScript/ch05/05_2_mother-child_age_difference.js
@@ -1,3 +1,9 @@
 /**
- * add notes here
+ * Mother-child age difference:
+ * 	- Using the example data set from this chapter, compute the average age difference
+ * 		between mothers and children (the age of the mother when the child is born). You
+ * 		can use the average function defined earlier in this chapter.
+ * 	- Note: that not all the mothers mentioned in the data are themselves present in the
+ * 					array. The byName object, which makes it easy to find a person’s object from
+ * 					their name, might be useful here.
  */
