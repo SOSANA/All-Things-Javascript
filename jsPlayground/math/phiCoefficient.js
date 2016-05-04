c8096c71f3eedb80376cf618805a277cbe4715b4
sosana <thesosana@gmail.com>
2016-05-04 17:47:51 -0600

    updating phi math example

--- jsPlayground/math/phiCoefficient.js
+++ jsPlayground/math/phiCoefficient.js
@@ -180,3 +180,35 @@ console.log('-------------');
 var tabled = tableFor('pizza', JOURNAL);
 console.log(tabled);
 console.log('this has been correlated: ' + phi(tabled));
+console.log('-------------');
+
+// using a for loop
+function gatherCorrelations(journal) {
+  var phis = {};
+  for (var entry = 0; entry < journal.length; entry++) {
+    var events = journal[entry].events;
+    for (var i = 0; i < events.length; i++) {
+      var event = events[i];
+      if (!(event in phis))
+        phis[event] = phi(tableFor(event, journal));
+    }
+  }
+  return phis;
+}
+
+console.log(gatherCorrelations(JOURNAL));
+console.log('-------------');
+
+// using a forEach loop
+function gatherCorrelations2(journal) {
+  var phis = {};
+  journal.forEach(function(entry) {
+    entry.events.forEach(function(event) {
+      if (!(event in phis))
+        phis[event] = phi(tableFor(event, journal));
+    });
+  });
+  return phis;
+}
+
+console.log(gatherCorrelations2(JOURNAL));
