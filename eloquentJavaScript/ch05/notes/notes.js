bcde3802ffeef3caa1fcf325d14c35cd6c9d1494
sosana <thesosana@gmail.com>
2016-05-04 15:27:50 -0600

    updating notes on first class functions

--- eloquentJavaScript/ch05/notes/notes.js
+++ eloquentJavaScript/ch05/notes/notes.js
@@ -1,9 +1,11 @@
 /**
  * Abstraction:
- * 	-
+ * 	- hide details and give us the ability to talk about problems at a higher (or more abstract) level
  *
  * Abstracting array traversal:
- * 	-
+ * 	- The word "traverse" means "to go or travel across or over"
+ * 	- It just means you need to iterate (go through each element (an element being a portion of data the
+ * 		size of whatever data type the array holds))
  *
  * Higher-order functions:
  * 	-
