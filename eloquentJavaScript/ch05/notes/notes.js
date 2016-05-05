15c330bd6c39a40ac9143b9a2a2dd06e81a5dab8
sosana <thesosana@gmail.com>
2016-05-05 15:57:51 -0600

    updating notes on H.O.F

--- eloquentJavaScript/ch05/notes/notes.js
+++ eloquentJavaScript/ch05/notes/notes.js
@@ -18,12 +18,17 @@
  *  	- You can even write functions that provide new types of control flow
  *  	- Passing along arguments, which wraps its argument in another function
  *
- * Passing along arguments:
- * 	-
- *
  * JSON:
- * 	-
- *
+ *  - JSON (pronounced “Jason”), which stands for JavaScript Object Notation. It is widely used as a data
+ * 		storage and communication format on the Web
+ *  - All property names have to be surrounded by double quotes, and only simple data expressions are
+ *  	allowed—no function calls, variables, or anything that involves actual computation. Comments are
+ *  	not allowed in JSON
+ * - JavaScript gives us functions, JSON.stringify and JSON.parse, that convert data from and to this format.
+ * 	 The first takes a JavaScript value and returns a JSON-encoded string. The second takes such a string and
+ * 	 converts it to the value it encodes.
+ * 	 - JSON.stringify converts from JavaScript to JSON
+ * 	 - JSON.parse converts from JSON to JavaScript
  * Filtering an array:
  * 	-
  *
