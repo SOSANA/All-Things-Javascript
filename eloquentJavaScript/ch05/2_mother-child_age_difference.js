110a21d49b40bd46513cdafea8ea10ce01d6c72b
sosana <thesosana@gmail.com>
2016-05-18 08:33:16 -0600

    updating mother / child avg example

--- eloquentJavaScript/ch05/2_mother-child_age_difference.js
+++ eloquentJavaScript/ch05/2_mother-child_age_difference.js
@@ -6,6 +6,14 @@
  * 	- Note: that not all the mothers mentioned in the data are themselves present in the
  * 					array. The byName object, which makes it easy to find a person’s object from
  * 					their name, might be useful here.
+ * 	- Hints:
+ * 		- Because not all elements in the ancestry array produce useful data (we can’t compute
+ * 			the age difference unless we know the birth date of the mother), we will have to apply
+ * 			filter in some manner before calling average. You could do it as a first pass, by defining
+ * 			a hasKnownMother function and filtering on that first. Alternatively, you could start by
+ * 			calling map and in your mapping function return either the age difference or null if no
+ * 			mother is known. Then, you can call filter to remove the null elements before passing the
+ * 			array to average.
  */
 
 /* eslint-disable */
