39d5729de6b61b38443e8c06c68a168bcb4c6c86
sosana <thesosana@gmail.com>
2016-05-27 14:10:13 -0600

    adding various notes related working javascript objects

--- eloquentJavaScript/ch06/notes/notes.js
+++ eloquentJavaScript/ch06/notes/notes.js
@@ -1,5 +1,67 @@
 /**
+ * Classical Vs Prototypal Inheritance:
+ *  - Inheritance
+ *    - one object gets access to the properites and methods of another object.
+ *  - Classical Inheritance
+ *    - commonly use in classical programming such as java, c++, php, etc
+ *    - considered verbose, uses friend, protected, private, interface
+ *  - Protypal Inheritance
+ *    - Simple, flexible, extensible and easy to understand
+ *    - two patterns of prototypal inheritance
+ *        - prototypal pattern of prototypal inheritance
+ *        - the constructor pattern of prototypal inheritance
+ *    - there are two ways of creating objects using prototypal inheritance
+ *        - create a brand new object.
+ *        - clone an existing object and extend it
+ *  - good explanation found here:
+ *    http://stackoverflow.com/questions/2800964/benefits-of-prototypal-inheritance-over-classical
  *
- * add notes here
+ * Methods:
+ *  -
  *
+ * Prototypes:
+ *  - all functions and arrays have prototype pointing to an object with those function methods
+ *  - all functions have a property called prototype that starts out as an empty object
+ *  - always use captial letter ex: function Person(firstname, lastname) {};
+ *  - prototype property is used only by the "new" operator. Make sure to always use "new"
+ *    assigning prototypes to objects as it will come back undefined
+ *  - understand the difference of using "new" and built-in functions
+ *    ex: var b = new Number(3); // creating a new object
+ *        var c = Numnber(3); // calling the function and getting an object back
+ *  - allows access to all objects just once, rather than every object taking up memory space
+ *    and getting a copy for every object ex: if you have a 1000 Person Objects with getFullName
+ *    methods you will have a 1000 getFullName methods taking up memory space rather than having
+ *    a prototype you will only have need one method
+ *
+ * Function Constructors:
+ *  - a normal function that is used to construct objects
+ *  - the 'this' variable points to a new empty object, and that object is returned from the
+ *    function automatically
+ *
+ * Overriding derived properties:
+ *  -
+ *
+ * Prototype interference:
+ *  -
+ *
+ * Prototype-less objects:
+ *  -
+ *
+ * Polymorphism:
+ *  -
+ *
+ * Object.keys:
+ *  -
+ *
+ * Inheritance:
+ *  -
+ *
+ * The instanceof operator:
+ *  -
+ *
+ * Getters and setters:
+ *  -
+ *
+ * Methods:
+ *  -
  */
