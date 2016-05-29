0d663762f0a661891afd164d4cf94af5f7591560
sosana <thesosana@gmail.com>
2016-05-29 17:29:25 -0600

    adding notes on using 'this' and using various methods call, bind, apply

--- eloquentJavaScript/ch06/notes/notes.js
+++ eloquentJavaScript/ch06/notes/notes.js
@@ -1,4 +1,16 @@
 /**
+ * .apply():
+ *  - similar to the call method, invokes the function and lets you set the 'this' keyword but only
+ *  	difference is it wants an array of parameters
+ *
+ * .bind():
+ *  - does not invoke the function, it creates a copy of the function and you can tell it what the
+ *    "this" variable is
+ *
+ * .call():
+ *  - unlike the bind method, call doesn't make a copy it actually executes or invokes it and
+ *  	decides what the "this" variable is
+ *  	
  * Classical Vs Prototypal Inheritance:
  *  - Inheritance
  *    - one object gets access to the properites and methods of another object.
