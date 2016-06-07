0994a39d02f02486abe3398604d49fcf465cd922
sosana <thesosana@gmail.com>
2016-06-07 15:15:56 -0600

    updating project

--- eloquentJavaScript/src/solutions/20_2_fixing_a_leak.js
+++
@@ -1,8 +0,0 @@
-// The file server examples in code/file_server.js and
-// code/file_server_promises.js already contain this fixed version.
-
-function urlToPath(url) {
-  var path = url;
-  var decoded = decodeURIComponent(path);
-  return "." + decoded.replace(/(\/|\\)\.\.(\/|\\|$)/g, "/");
-}
