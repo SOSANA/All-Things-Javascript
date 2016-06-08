8dd18ec219e412fa9af03204a28fb731bdc55a86
sosana <thesosana@gmail.com>
2016-06-08 13:39:35 -0600

    adding project notes

---
+++ practical-javascript/notes.js
@@ -0,0 +1,9 @@
+/**
+ * Requirements:
+ *  - a common practice in the real world is to break down tasks into small junks
+ *  - it helps with not getting lost and side tracked
+ *  - makes it easy to see if our app is working the way we want it to working
+ *  - works great when having a team of developers collaborating together
+ *  - ex: ./v1Requirements
+ *
+ */
