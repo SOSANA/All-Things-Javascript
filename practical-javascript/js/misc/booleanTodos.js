c6ad0ee0dfd14e72c2f8a306101d8b3653ee9d04
sosana <thesosana@gmail.com>
2016-06-08 18:36:09 -0600

    todoList1.addTodo1 property now adds objects

--- practical-javascript/js/misc/booleanTodos.js
+++ practical-javascript/js/misc/booleanTodos.js
@@ -1,8 +1,7 @@
 /**
  * - boolean:
  * 		- true or false
- * - basic example of creating todo object
- * - showing two ways of creating objects via object literal and function constructor
+ * - basic example of using boolean values
  * - Action Items:
  * 		[ ] todoList.addTodo should add objects
  * 		[ ] todoList.changeTodo should change the todoText property
@@ -12,25 +11,22 @@
 /* eslint-disable */
 'use strict';
 
-// using object literal
-// it should store the todos array on an object
 var todoList1 = {
-  todos1: ['item 1', 'item 2', 'item 3'],
-  // it should have a displayTodos method
-  displayTodos1: function() {
+  todos1: [],
+  displayTodos1: function(todos1) {
     console.log('My todos:', this.todos1);
   },
-  // it should have a addTodo method
-  addTodo: function(input) {
-    this.todos1.push(input);
+  addTodo: function(todoText) {
+    this.todos1.push({
+      todoText: todoText,
+      completed: false
+    });
     this.displayTodos1();
   },
-  // it should have a changeTodo method
   changeTodo: function(pos, val) {
     this.todos1[pos] = val;
     this.displayTodos1();
   },
-  // it should have a deleteTodo method
   deleteTodo: function(start, deleteCount) {
     this.todos1.splice(start, deleteCount);
     this.displayTodos1();
@@ -39,7 +35,7 @@ var todoList1 = {
 console.log('This is our object Literal todo list:');
 console.log('-----------------------');
 console.log('This is our displayTodos():');
-todoList1.displayTodos1(); // My todos: [ 'item 1', 'item 2', 'item 3' ]
+todoList1.displayTodos1(['item 1', 'item 2', 'item 3']); // My todos: [ 'item 1', 'item 2', 'item 3' ]
 console.log('This is our addTodo():');
 todoList1.addTodo('new todo'); // My todos: [ 'item 1', 'item 2', 'item 3', 'new todo' ]
 console.log('This is our changeTodo():');
@@ -50,33 +46,25 @@ console.log('-----------------------');
 console.log('-----------------------');
 
 
-
-// using function constructor
-// it should store the todos array on an object
 function TodoList(todos) {
   this.todos = todos;
 }
-// it should have a displayTodos method
 TodoList.prototype.displayTodos = function() {
   console.log('My todos:', this.todos);
 };
-// it should have a addTodo method
 TodoList.prototype.addTodo = function(input) {
   this.todos.push(input);
   this.displayTodos();
 };
-// it should have a changeTodo method
 TodoList.prototype.changeTodo = function(pos, val) {
   this.todos[pos] = val;
   this.displayTodos();
 };
-// it should have a deleteTodo method
 TodoList.prototype.deleteTodo = function(start, deleteCount) {
   this.todos.splice(start, deleteCount);
   this.displayTodos();
 };
 
-// lets reassign it to myTodoList and inherit from TodoList
 var myTodoList = new TodoList(['item 1', 'item 2', 'item 3']);
 
 console.log('This is our function constructor todo list:');
