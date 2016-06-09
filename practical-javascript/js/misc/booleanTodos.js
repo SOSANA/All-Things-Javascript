7e553d35ecf78d6e73cc4abb52087424a3901d12
sosana <thesosana@gmail.com>
2016-06-08 18:42:02 -0600

    updating our addTodo method to use objects

--- practical-javascript/js/misc/booleanTodos.js
+++ practical-javascript/js/misc/booleanTodos.js
@@ -11,31 +11,32 @@
 /* eslint-disable */
 'use strict';
 
+// our object literal
 var todoList1 = {
-  todos1: [],
-  displayTodos1: function(todos1) {
-    console.log('My todos:', this.todos1);
+  todos: [],
+  displayTodos: function() {
+    console.log('My todos:', this.todos);
   },
   addTodo: function(todoText) {
-    this.todos1.push({
+    this.todos.push({
       todoText: todoText,
       completed: false
     });
-    this.displayTodos1();
+    this.displayTodos();
   },
   changeTodo: function(pos, val) {
-    this.todos1[pos] = val;
-    this.displayTodos1();
+    this.todos[pos] = val;
+    this.displayTodos();
   },
   deleteTodo: function(start, deleteCount) {
     this.todos1.splice(start, deleteCount);
-    this.displayTodos1();
+    this.displayTodos();
   }
 };
 console.log('This is our object Literal todo list:');
 console.log('-----------------------');
 console.log('This is our displayTodos():');
-todoList1.displayTodos1(['item 1', 'item 2', 'item 3']); // My todos: [ 'item 1', 'item 2', 'item 3' ]
+todoList1.displayTodos(['item 1', 'item 2', 'item 3']); // My todos: [ 'item 1', 'item 2', 'item 3' ]
 console.log('This is our addTodo():');
 todoList1.addTodo('new todo'); // My todos: [ 'item 1', 'item 2', 'item 3', 'new todo' ]
 console.log('This is our changeTodo():');
@@ -45,15 +46,18 @@ todoList1.deleteTodo(0, 1); // My todos: [ 'item 2', 'item 3', 'new todo' ]
 console.log('-----------------------');
 console.log('-----------------------');
 
-
+// our function constructor
 function TodoList(todos) {
   this.todos = todos;
 }
 TodoList.prototype.displayTodos = function() {
   console.log('My todos:', this.todos);
 };
-TodoList.prototype.addTodo = function(input) {
-  this.todos.push(input);
+TodoList.prototype.addTodo = function(todoText) {
+  this.todos.push({
+    todoText: todoText,
+    completed: false
+  });
   this.displayTodos();
 };
 TodoList.prototype.changeTodo = function(pos, val) {
