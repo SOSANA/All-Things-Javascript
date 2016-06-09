bee096c31062ce50d1fcae89410096f124d0df0d
sosana <thesosana@gmail.com>
2016-06-08 18:04:57 -0600

    updating notes on action items aka requirements

--- practical-javascript/notes.js
+++ practical-javascript/notes.js
@@ -1,5 +1,5 @@
 /**
- * Requirements:
+ * Action Items aka 'Project Requirements':
  *  - a common practice in the real world is to break down tasks into small junks
  *  - it helps with not getting lost and side tracked
  *  - makes it easy to see if our app is working the way we want it to working
