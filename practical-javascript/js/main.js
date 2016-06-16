43058f9435374f28394558770596a2cf48dd32fd
sosana <thesosana@gmail.com>
2016-06-15 20:39:23 -0600

    created new object for holding our views, removed unneeded method in todoList object, add new methods handler object for user interactions

--- practical-javascript/js/main.js
+++ practical-javascript/js/main.js
@@ -1,38 +1,21 @@
 /* eslint-disable */
 'use strict';
 
+// creating a object for our data and logic
 var todoList = {
   todos: [],
-  displayTodos: function() {
-    var ArrayLength = this.todos.length;
-    if (ArrayLength === 0) {
-      console.log('Your todo list is empty!');
-    } else {
-      console.log('My Todos:');
-      for (var i = 0; i < ArrayLength; i++) {
-        if (this.todos[i].completed === true) {
-          console.log('[X]', this.todos[i].todoText);
-        } else {
-            console.log('[ ]', this.todos[i].todoText);
-        }
-      }
-    }
-  },
   addTodo: function(todoText) {
     this.todos.push({
       todoText: todoText,
       completed: false
     });
-    this.displayTodos();
   },
   changeTodo: function(pos, todoText) {
     this.todos[pos].todoText = todoText;
-    this.displayTodos();
   },
   toggleCompleted: function(pos) {
     var todo = this.todos[pos];
     todo.completed = !todo.completed;
-    this.displayTodos();
   },
   toggleAll: function() {
     var totalTodos = this.todos.length;
@@ -53,20 +36,15 @@ var todoList = {
         this.todos[k].completed = true;
       }
     }
-
-    this.displayTodos();
   },
   deleteTodo: function(pos) {
+    // splice takes two arguments, a starting position and deleteCount
     this.todos.splice(pos, 1);
-    this.displayTodos();
   }
 };
 
-// adding event listeners
+// creating an object for our event handlers soley for user interactions
 var handlers = { // jshint ignore:line
-  displayTodos: function() {
-    todoList.displayTodos();
-  },
   addTodo: function() {
     // get access to addTodoTextInput element
     var addTodoTextInput = document.getElementById('addTodoTextInput');
@@ -74,6 +52,7 @@ var handlers = { // jshint ignore:line
     todoList.addTodo(addTodoTextInput.value);
     // clears input field after we click button
     addTodoTextInput.value = '';
+    view.displayTodos();
   },
   changeTodo: function() {
     // get access to elements
@@ -84,19 +63,53 @@ var handlers = { // jshint ignore:line
     // clears input field after we click button
     changeTodoPositionInput.value = '';
     changeTodoTextInput.value = '';
+    view.displayTodos();
   },
   toggleCompleted: function() {
     var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
     todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
     toggleCompletedPositionInput.value = '';
+    view.displayTodos();
   },
   toggleAll: function() {
     todoList.toggleAll();
+    view.displayTodos();
   },
   deleteTodo: function() {
     var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
     todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
     deleteTodoPositionInput.value = '';
+    view.displayTodos();
+  }
+};
+
+// creating an object for only what the view looks like
+// doesn't have any logic, it doesn't change any data in the app
+var view = { // jshint ignore:line
+  displayTodos: function() {
+    // grabbing a reference to ul element
+    var todosUl = document.querySelector('ul');
+    // making sure our for loop starts at 0 everytime so we set innerHTML
+    // to nothing before we start entering for loop
+    todosUl.innerHTML = '';
+    // creating a li element for each todoText in the todos array
+    for (var i = 0; i < todoList.todos.length; i++) {
+      // creating a todoText li element
+      var todoLi = document.createElement('li');
+      var todo = todoList.todos[i];
+      var todoTextWithCompletion = '';
+
+      if (todo.completed === true) {
+        todoTextWithCompletion = '[x] ' + todo.todoText;
+      } else {
+        todoTextWithCompletion = '[ ] ' + todo.todoText;
+      }
+
+      // there is a text property on these elements and setting it to another value
+      todoLi.textContent = todoTextWithCompletion;
+      // appending child element to our selector
+      todosUl.appendChild(todoLi);
+    }
   }
 };
 
